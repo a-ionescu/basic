32773f85a8e8da3ae750c78b7a050584b0396ce4
a-ionescu <aionescu@luxoft.com>
2017-07-19 22:55:15 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/aionescu3/basic/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.aionescu3.basic;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.aionescu3.basic", appContext.getPackageName());
+    }
+}
